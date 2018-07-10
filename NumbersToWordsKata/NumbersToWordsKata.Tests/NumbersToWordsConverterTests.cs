b354f4a3fb90f6a6569eac36cc0ce5cb61bd555f
Monique Govender <MoniqueG@SAHOMELOANS.COM>
2018-07-10 15:56:10 +0200

    started with few tests

---
+++ NumbersToWordsKata/NumbersToWordsKata.Tests/NumbersToWordsConverterTests.cs
@@ -0,0 +1,80 @@
+﻿using NUnit.Framework;
+
+namespace NumbersToWordsKata.Tests
+{
+    [TestFixture]
+    public class NumbersToWordsConverterTests
+    {
+        [TestCase(0, "zero")]
+        [TestCase(1, "one")]
+        [TestCase(2, "two")]
+        [TestCase(3, "three")]
+        [TestCase(4, "four")]
+        [TestCase(5, "five")]
+        [TestCase(6, "six")]
+        [TestCase(7, "seven")]
+        [TestCase(8, "eight")]
+        [TestCase(9, "nine")]
+        public void Convert_GivenUnits_ShouldReturnUnitsInWords(int input, string expected)
+        {
+            //Arrange
+            var sut = CreateNumbersToWordsConverter();
+
+            //Act
+            var actual = sut.Convert(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestCase(10, "ten")]
+        [TestCase(20, "twenty")]
+        [TestCase(30, "thirty")]
+        [TestCase(40, "forty")]
+        [TestCase(50, "fifty")]
+        [TestCase(60, "sixty")]
+        [TestCase(70, "seventy")]
+        [TestCase(80, "eighty")]
+        [TestCase(90, "ninety")]
+        public void Convert_GivenTens_ShouldReturnTensInWords(int input, string expected)
+        {
+            //Arrange
+            var sut = CreateNumbersToWordsConverter();
+
+            //Act
+            var actual = sut.Convert(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(11, "eleven")]
+        [TestCase(12, "twelve")]
+        [TestCase(13, "thirteen")]
+        [TestCase(14, "fourteen")]
+        [TestCase(15, "fifteen")]
+        [TestCase(16, "sixteen")]
+        [TestCase(17, "seventeen")]
+        [TestCase(18, "eighteen")]
+        [TestCase(19, "nineteen")]
+        public void Convert_GivenATeenNumber_ShouldReturnInWords(int input, string expected)
+        {
+            //Arrange
+            var sut = CreateNumbersToWordsConverter();
+
+            //Act
+            var actual = sut.Convert(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        private static NumbersToWordsConverter CreateNumbersToWordsConverter()
+        {
+
+            return new NumbersToWordsConverter();
+        }
+
+    }
+}
---
+++ NumbersToWordsKata/NumbersToWordsKata/NumbersToWordsConverter.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumbersToWordsKata
+{
+    public class NumbersToWordsConverter
+    {
+        public string Convert(int input)
+        {
+            var Units = GetUnitsDictionary();
+            foreach (KeyValuePair<int, string> item in Units)
+            {
+                if (item.Key == input)
+                {
+                    return item.Value;
+                }
+            }
+
+            var Tens = GetTensDictionary();
+            foreach (KeyValuePair<int, string> item in Tens)
+            {
+                if (item.Key == input)
+                {
+                    return item.Value;
+                }
+            }
+
+            var Teens = GetTeenDictionary();
+            foreach (KeyValuePair<int, string> item in Teens)
+            {
+                if (item.Key == input)
+                {
+                    return item.Value;
+                }
+            }
+            return null;
+
+        }
+
+        private static Dictionary<int, string> GetTeenDictionary()
+        {
+            Dictionary<int, string> Teens = new Dictionary<int, string>();
+            Teens.Add(11, "eleven");
+            Teens.Add(12, "twelve");
+            Teens.Add(13, "thirteen");
+            Teens.Add(14, "fourteen");
+            Teens.Add(15, "fifteen");
+            Teens.Add(16, "sixteen");
+            Teens.Add(17, "seventeen");
+            Teens.Add(18, "eighteen");
+            Teens.Add(19, "nineteen");
+            return Teens;
+        }
+
+        private static Dictionary<int, string> GetTensDictionary()
+        {
+            Dictionary<int, string> Tens = new Dictionary<int, string>();
+            Tens.Add(10, "ten");
+            Tens.Add(20, "twenty");
+            Tens.Add(30, "thirty");
+            Tens.Add(40, "forty");
+            Tens.Add(50, "fifty");
+            Tens.Add(60, "sixty");
+            Tens.Add(70, "seventy");
+            Tens.Add(80, "eighty");
+            Tens.Add(90, "ninety");
+            return Tens;
+        }
+
+        private static Dictionary<int, string> GetUnitsDictionary()
+        {
+            Dictionary<int, string> Units = new Dictionary<int, string>();
+            Units.Add(0, "zero");
+            Units.Add(1, "one");
+            Units.Add(2, "two");
+            Units.Add(3, "three");
+            Units.Add(4, "four");
+            Units.Add(5, "five");
+            Units.Add(6, "six");
+            Units.Add(7, "seven");
+            Units.Add(8, "eight");
+            Units.Add(9, "nine");
+            return Units;
+        }
+    }
+}
